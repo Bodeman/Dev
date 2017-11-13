ff950a041382de78047f39986c4ed5dad9f6d3e4
rduart <rduart@users.noreply.github.com>
2017-11-13 10:00:04 -0800

    Corrected to look for code 302

--- SpringPOC/scripts/basic_health_check.sh
+++ SpringPOC/scripts/basic_health_check.sh
@@ -4,8 +4,8 @@ echo " Start Health Check Script"
 
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://ec2-52-11-175-102.us-west-2.compute.amazonaws.com:8080`
-  if [ "$HTTP_CODE" == "200" ]; then
+  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://ec2-52-11-175-102.us-west-2.compute.amazonaws.com:8080/cammis`
+  if [ "$HTTP_CODE" == "320" ]; then
     echo "Successfully pulled root page."
     exit 0;
   fi
