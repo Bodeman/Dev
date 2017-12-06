7df678b6bfbfa003c36de1c1fedabbffc11e691e
rduart <rduart@users.noreply.github.com>
2017-12-06 11:09:51 -0800

    Update basic_health_check.sh
    Correct issues with hardcoded IP address.

--- SpringPOC/scripts/basic_health_check.sh
+++ SpringPOC/scripts/basic_health_check.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+myip=
+while IFS=$': \t' read -a line ;do
+  [ -z "${line%inet}" ] && ip=${line[${#line[1]}>4?1:2]} &&
+    [ "${ip#127.0.0.1}" ] && myip=$ip
+done< <(LANG=C /sbin/ifconfig)
+echo $myip
+
 echo " Start Health Check Script"
 
 for i in `seq 1 10`;
 do
-  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://158.96.16.199:8080/cammis`
+  HTTP_CODE=`curl --write-out '%{http_code}' -o /dev/null -m 10 -q -s http://$myip:8080/cammis`
   if [ "$HTTP_CODE" == "302" ]; then
     echo "Successfully pulled root page."
     exit 0;
