d25bacc72716fe87258ea1c02d8f5206c34fc4d2
rduart <rduart@users.noreply.github.com>
2018-03-20 21:10:46 +0000

    PTP-26 This is a PTP-26  test

--- SpringPOC/scripts/basic_health_check.sh
+++ SpringPOC/scripts/basic_health_check.sh
@@ -2,7 +2,7 @@
 
 myip=
 while IFS=$': \t' read -a line ;do
-  [ -z "${line%inet}" ] && ip=${line[${#line[1]}>4?1:2]} &&
+  [ -z "${line%inet}" ] && ip=${line[${#line[1]}>4?1:2]} &&  
     [ "${ip#127.0.0.1}" ] && myip=$ip
 done< <(LANG=C /sbin/ifconfig)
 echo $myip
