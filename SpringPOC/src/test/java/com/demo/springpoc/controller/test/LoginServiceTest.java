54b6a321f7b8d5062a23401b44cbd6022933e57f
rduart <rduart@users.noreply.github.com>
2018-03-22 20:43:08 +0000

    PTP-25 Changed unit test case PWD to C

--- SpringPOC/src/test/java/com/demo/springpoc/controller/test/LoginServiceTest.java
+++ SpringPOC/src/test/java/com/demo/springpoc/controller/test/LoginServiceTest.java
@@ -36,7 +36,7 @@ public class LoginServiceTest {
 		Login login = new Login();
 		
 		String user = "a";
-		String pass = "ax";
+		String pass = "c";
 		
 		loginBean.setUsername(user);
 		loginBean.setPassword(pass);
