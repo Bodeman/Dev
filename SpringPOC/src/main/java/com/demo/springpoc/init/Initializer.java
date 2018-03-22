fec3f0e50f32edcb251815019e84e2b074e07aab
rduart <rduart@users.noreply.github.com>
2018-03-22 19:03:00 +0000

    PTP-26 Added spaces tof file

--- SpringPOC/src/main/java/com/demo/springpoc/init/Initializer.java
+++ SpringPOC/src/main/java/com/demo/springpoc/init/Initializer.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
-import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.WebApplicationInitializer;    
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
