91f3c8b6cf6759aab37ac65793922be988c0ec9d
rduart <rduart@users.noreply.github.com>
2018-04-03 15:01:08 -0700

    PTP-27 Jira Bug Check

--- SpringPOC/src/test/java/com/demo/springpoc/controller/test/LoginServiceTest.java
+++ SpringPOC/src/test/java/com/demo/springpoc/controller/test/LoginServiceTest.java
@@ -2,7 +2,7 @@ package com.demo.springpoc.controller.test;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.when;    
 
 import org.junit.Before;
 import org.junit.Test;
