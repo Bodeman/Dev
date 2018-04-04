7c350610ef1668e493336e4fb1fdc0b6df924c7a
rduart <rduart@users.noreply.github.com>
2018-04-04 09:21:01 -0700

    PTP-13 Inserted Complier Error to test Jira

--- SpringPOC/src/main/java/com/demo/springpoc/init/Initializer.java
+++ SpringPOC/src/main/java/com/demo/springpoc/init/Initializer.java
@@ -2,7 +2,7 @@ package com.demo.springpoc.init;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.Dynamic;
+//import javax.servlet.ServletRegistration.Dynamic;
 
 import org.springframework.web.WebApplicationInitializer;    
 import org.springframework.web.context.ContextLoaderListener;
